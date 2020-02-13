86f5d85b242ceef24301f7d09f5594531c9de436
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2020-02-14 00:09:20 +0900

    add network func

---
+++ network.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class neuralnetwork:
+    def __init__(self):
+        pass
+
+    def load_network(self):
+        pass
+
+    def save_network(self):
+        pass
+
+    def get_network(self):
+        return None
+
